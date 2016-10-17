c6673b5453808d285cd300209f1f85153abd5852
Neil Ashton <neil.m.ashton@gmail.com>
2016-10-17 07:35:16 -0400

    Using gulp-pug instead of straight pug in inner stream

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var fm = require('gulp-front-matter');
 var marked = require('gulp-marked');
+var pug = require('gulp-pug');
+var rename = require('gulp-rename');
 var tap = require('gulp-tap');
 var fs = require('fs');
-var pug = require('pug');
+var path = require('path');
 
 var DIST = 'dist';
 var DEFAULT_TEMPLATE = 'templates/page.pug'
@@ -20,12 +22,17 @@ gulp.task('pages', function () {
     .pipe(fm())
     .pipe(marked())
     .pipe(tap(function (file) {
+      var templateName = file.frontMatter.template || DEFAULT_TEMPLATE;
       var contents = file.contents.toString();
       var data = Object.assign({}, file.frontMatter, {contents: contents});
-      var rendered = pug.renderFile(file.frontMatter.template || DEFAULT_TEMPLATE, data)
-      file.contents = new Buffer(rendered, 'utf-8');
-    }))
-    .pipe(gulp.dest(DIST));
+
+      gulp.src(templateName)
+        .pipe(pug({
+          locals: data
+        }))
+        .pipe(rename(path.basename(file.path).replace(/\.md$/, '.html')))
+        .pipe(gulp.dest(DIST));
+    }));
 });
 
 gulp.task('default', ['pages', 'connect']);
